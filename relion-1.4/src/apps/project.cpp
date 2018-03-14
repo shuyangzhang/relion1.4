a5ed6535cd1d5648f3b3c2ee0039a6a4c527631c
zhangshuyang <zhangshuyang@outlook.com>
2018-03-14 15:13:48 +0000

    sequence is griddingcorrect getspectrum centerFFT

--- relion-1.4/src/apps/project.cpp
+++ relion-1.4/src/apps/project.cpp
@@ -307,13 +307,14 @@ public:
                 // do gridding correct after inverse fourier transform
                 projector.griddingCorrect(img() );
 
-                // Shift the image back to the center...
-                CenterFFT(img(), false);
 
                 //print the power spectrum of image has been rotated when the image back to the center
                 MultidimArray<DOUBLE> spectrum_for_compare;
                 getSpectrum(img(), spectrum_for_compare);
                 std::cout << " the spectrum is " << spectrum_for_compare << std::endl;
+
+                // Shift the image back to the center...
+                CenterFFT(img(), false);
 
 
                 // Subtract the projection from the corresponding experimental image
